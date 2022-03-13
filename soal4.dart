777e710cb76216bdc1d2945fde4e366f28a26be1
Syai Record <syairecord@gmail.com>
2022-03-13 17:32:19 +0700

    fix bug

--- soal4.dart
+++ soal4.dart
@@ -1,14 +1,22 @@
 void main() {
-  String input = "12345";
-  var word = input.split('');
+  String input = "124345h";
+  List<String> word = input.split('');
+  // print(word);
+  print(input);
 
-  for (int i = 0; i < word.length; i++) {
-    var value = word[i];
-    if (int.parse(value) % 2 == 0) {
-      word.insert(i + 1, '-');
-    }
-  }
+  // for (int i = 0; i < word.length; i++) {
+  //   var value = word[i];
+  //   var value2;
+  //   if (i + 1 == word.length) {
+  //     value2 = 1;
+  //   } else {
+  //     value2 = word[i + 1];
+  //   }
+  //   if (int.parse(value) % 2 == 0 && int.parse(value2) % 2 == 0) {
+  //     //word.insert(i + 1, int.parse('-'));
+  //   }
+  // }
 
-  var output = word.join();
-  print(output);
+  // var output = word.join();
+  // print(output);
 }
